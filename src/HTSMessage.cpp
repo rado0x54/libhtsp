4622ff7a15941c83d0196edbe8f1318ee2af31b3
Martin Riedel <web@riedel-privat.de>
2017-09-20 02:55:41 +0200

    Include string.h import.

--- src/HTSMessage.cpp
+++ src/HTSMessage.cpp
@@ -22,6 +22,7 @@
 
 #include <arpa/inet.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 //enum {
@@ -68,6 +69,7 @@ HtsMap::HtsMap(uint32_t /*length*/, void *buf) {
     tmpbuf += 1;
 
     int64_t mlen = ntohl(*((uint32_t *) tmpbuf));
+    tmpbuf += sizeof(uint32_t);
     tmpbuf += sizeof(uint32_t);
 
     if (nlen > 0) {
@@ -320,13 +322,13 @@ HtsBin::HtsBin(uint32_t /*length*/, void *buf) {
     }
 
     data_buf = malloc(data_length);
-    std::memcpy(data_buf, tmpbuf, data_length);
+    memcpy(data_buf, tmpbuf, data_length);
 }
 
 void HtsBin::getBin(uint32_t *len, void **buf) const {
     *len = data_length;
     void *mem = malloc(data_length);
-    std::memcpy(mem, data_buf, data_length);
+    memcpy(mem, data_buf, data_length);
     *buf = mem;
 }
 
@@ -336,7 +338,7 @@ void HtsBin::setBin(uint32_t len, void *buf) {
 
     data_length = len;
     data_buf = malloc(len);
-    std::memcpy(data_buf, buf, len);
+    memcpy(data_buf, buf, len);
 }
 
 HtsBin::~HtsBin() {
@@ -438,7 +440,7 @@ void HtsMap::Serialize(void *buf) {
     tmpbuf += 6;
 
     if (getName().length() > 0) {
-        std::memcpy(tmpbuf, getName().c_str(), getName().length());
+        memcpy(tmpbuf, getName().c_str(), getName().length());
         tmpbuf += getName().length();
     }
 
@@ -471,7 +473,7 @@ void HtsList::Serialize(void *buf) {
     tmpbuf += 6;
 
     if (getName().length() > 0) {
-        std::memcpy(tmpbuf, getName().c_str(), getName().length());
+        memcpy(tmpbuf, getName().c_str(), getName().length());
         tmpbuf += getName().length();
     }
 
@@ -507,7 +509,7 @@ void HtsInt::Serialize(void *buf) {
     tmpbuf += 6;
 
     if (getName().length() > 0) {
-        std::memcpy(tmpbuf, getName().c_str(), getName().length());
+        memcpy(tmpbuf, getName().c_str(), getName().length());
         tmpbuf += getName().length();
     }
 
@@ -532,11 +534,11 @@ void HtsStr::Serialize(void *buf) {
     tmpbuf += 6;
 
     if (getName().length() > 0) {
-        std::memcpy(tmpbuf, getName().c_str(), getName().length());
+        memcpy(tmpbuf, getName().c_str(), getName().length());
         tmpbuf += getName().length();
     }
 
-    std::memcpy(tmpbuf, data.c_str(), data.length());
+    memcpy(tmpbuf, data.c_str(), data.length());
 }
 
 uint32_t HtsBin::calcSize() {
@@ -553,9 +555,9 @@ void HtsBin::Serialize(void *buf) {
     tmpbuf += 6;
 
     if (getName().length() > 0) {
-        std::memcpy(tmpbuf, getName().c_str(), getName().length());
+        memcpy(tmpbuf, getName().c_str(), getName().length());
         tmpbuf += getName().length();
     }
 
-    std::memcpy(tmpbuf, data_buf, data_length);
+    memcpy(tmpbuf, data_buf, data_length);
 }
